869c29552029afe9035867dcb9755e537cd40ba1
Dhruv Parsana <dhruvparsana40@gmail.com>
2026-03-13 14:37:32 +0530

    Update HooksGST.jsx

--- HooksGST.jsx
+++ HooksGST.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Style.css'
 function HooksGST(e){
   const [amount, setAmount] = React.useState('')
   const [gst, setGSt] = React.useState('')
@@ -33,35 +34,29 @@ function HooksGST(e){
   }
 
   return(<>
+    <div className="gst-container">
+      <h2 className="gst-title">GST Calculator</h2>
+      Initial Amount :
+      <input type='text' onChange={(e)=>setAmount(e.target.value)} />
 
-    Initial Amount : <input type='text' onChange={(e)=>setAmount(e.target.value)} /><br/>
+      <select onChange={(e) => setGSt(e.target.value)}>
+        <option value="-1">Select GST</option>
+        <option>0.25%</option>
+        <option>3%</option>
+        <option>5%</option>
+        <option>12%</option>
+        <option>18%</option>
+        <option>28%</option>
+      </select>
 
-    {/* Dropdown button */}
-    <select onChange={(e) => setGSt(e.target.value)}>
-      <option value="-1" >Select</option>
-      <option> 0.25% </option>
-      <option> 3% </option>
-      <option> 5% </option>
-      <option> 12% </option>
-      <option> 18% </option>
-      <option> 28% </option>
-    </select>
+      <input type='button' value='Calculate GST' onClick={onSubmit}/>
 
-    {/* radio button */}
-    {/* <label onChange={(e)=> setGSt(e.target.value)}>
-      <input type='radio' name='option' value='0.25%'/> 0.25% <br/>
-      <input type='radio' name='option' value='3%'/> 3% <br/>
-      <input type='radio' name='option' value='5%'/> 5% <br/>
-      <input type='radio' name='option' value='12%'/> 12% <br/>
-      <input type='radio' name='option' value='18%'/> 18% <br/>
-      <input type='radio' name='option' value='28%'/> 28% <br/>
-    </label> */}
-
-    <input type='button' value='GST' onClick={onSubmit}/><br/>
-    Net Amount : {amount}<br/>
-    GST Amount : {ans1}<br/>
-    Total Amount : {ans2}<br/>
-    
+      <div className="result">
+      Net Amount : <span>{amount}</span><br/>
+      GST Amount : <span>{ans1}</span><br/>
+      Total Amount : <span>{ans2}</span>
+      </div>
+</div>
   </>)
 }
 export default HooksGST
